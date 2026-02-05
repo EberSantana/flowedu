2aad4d743f762419d82add04ec148c67ff758393
EberSantana <root@srv1271715.hstgr.cloud>
2026-02-05 01:28:42 +0000

    chore: Remove arquivos SQL temporários

--- add_special_kimono.sql
+++
@@ -1,2 +0,0 @@
--- Adicionar campo para kimono especial desbloqueável
-ALTER TABLE students ADD COLUMN specialKimono VARCHAR(30) DEFAULT 'none' AFTER avatarPose;
--- query_unnamed_users.sql
+++
@@ -1,4 +0,0 @@
-SELECT id, openId, name, email, role, loginMethod, createdAt, lastSignedIn 
-FROM users 
-WHERE name IS NULL OR name = '' OR name = 'Sem nome'
-ORDER BY createdAt DESC;
